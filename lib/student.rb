560f187efd8f1a6f338138933e6a339b368b7baa
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-12-19 05:06:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,15 +71,24 @@ class Student
     end 
   end 
   
-  def self.first_X_students_in_grade_10
-    
-  end 
-  
-  def self.first_student_in_grade_10 
-    
+  def self.first_X_students_in_grade_10(number)
+    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT ?;"
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
   end
   
-  def self.all_students_in_grade_X
-    
-  end 
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT 1;"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = ? ORDER BY students.id;"
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
